850245c53075590d6cc8441cb75db9b77b0070e8
michael <michael@greatfeatservices.com>
2018-11-08 18:56:47 +0800

    拖拽排序

--- src/pages/writer.vue
+++ src/pages/writer.vue
@@ -8,6 +8,7 @@
                     :class="{hidden:obj.isHidden,active:obj.isActive}"
                     @click="listTitleClick(index)"
                     @mousedown="listTitleDown($event,obj.name,index,obj.isActive)"
+                    :style="{transform: 'translate3d(0,'+ obj.y +'px,0)'}"
                 >
                     <span>{{obj.name}}</span>
                     <div v-if="obj.isActive"><i class="el-icon-setting"></i></div>
@@ -46,49 +47,43 @@
                 },
                 // 列表title
                 listTitle: [
-                    {name: 'Linux', isHidden: false, isActive: false},
-                    {name: 'Mac', isHidden: false, isActive: false},
-                    {name: 'Windows', isHidden: false, isActive: false},
-                    {name: 'Html', isHidden: false, isActive: false},
-                    {name: 'Css', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
-                    {name: 'Javascript', isHidden: false, isActive: false},
+                    {index: 0, name: 'Linux', isHidden: false, isActive: false, y: 0},
+                    {index: 1, name: 'Mac', isHidden: false, isActive: false, y: 0},
+                    {index: 2, name: 'Windows', isHidden: false, isActive: false, y: 0},
+                    {index: 3, name: 'Css', isHidden: false, isActive: false, y: 0},
+                    {index: 4, name: 'Html', isHidden: false, isActive: false, y: 0},
+                    {index: 5, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 6, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 7, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 8, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 9, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 10, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 11, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 12, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 13, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 14, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 15, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 16, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 17, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 18, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 19, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 20, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 21, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 22, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 23, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 24, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 25, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 26, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 27, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 28, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 29, name: 'Javascript', isHidden: false, isActive: false, y: 0},
+                    {index: 30, name: 'Javascript', isHidden: false, isActive: false, y: 0},
                 ],
                 // 列表scrollTop
                 writeLeftTop: 0,
             }
         },
-        computed : {},
+        computed: {},
         methods: {
             // 列表 title 点击高亮
             listTitleClick(index) {
@@ -104,6 +99,8 @@
                 let top = event.clientY - event.currentTarget.offsetTop + self.writeLeftTop;
                 let moveLeft = event.clientX - left;
                 let moveTop = event.clientY - top;
+                let several = 0;
+                let flagStr = '';
                 document.onmousemove = function (e) {
                     let x = e.clientX - left;
                     let y = e.clientY - top - moveTop;
@@ -119,18 +116,31 @@
                     };
                     // 向上滑动
                     if (y < 0) {
-                        console.log('向上滑动')
-                        // if () {
-                        //
-                        // }
+                        if (-y % 20 > 1 && -y < (index * 40 + 20)) {
+                            let num = Math.floor((-y - 1 - 20) / 40 + 1);
+                            if (num !== 0) {
+                                self.listTitle[index].index = index - num;
+                                several = num;
+                                flagStr = 'up';
+                                for (let i = 1; i <= num; i++) {
+                                    self.listTitle[index - i].y = 40;
+                                }
+                            }
+                        }
                     }
                     // 向下滑动
                     else if (y > 0) {
-                        console.log('向下滑动')
-                    }
-                    // 原地不动
-                    else {
-                        console.log('原地不动')
+                        if (y % 20 > 1) {
+                            let num = Math.floor((y - 1 - 20) / 40 + 1);
+                            if (num !== 0) {
+                                self.listTitle[index].index = index + num;
+                                several = num;
+                                flagStr = 'down';
+                                for (let i = 1; i <= num; i++) {
+                                    self.listTitle[index + i].y = -40;
+                                }
+                            }
+                        }
                     }
                     return false;
                 };
@@ -139,11 +149,39 @@
                     self.specialLi.isShow = false;
                     document.onmousemove = null;
                     document.onmouseup = null;
+                    for (let i = 1; i <= several; i++) {
+                        // 向上
+                        if (flagStr === 'up') {
+                            self.listTitle[index - i].index = self.listTitle[index - i].index + 1;
+                        }
+                        // 向下
+                        if (flagStr === 'down') {
+                            self.listTitle[index + i].index = self.listTitle[index + i].index - 1;
+                        }
+                    }
+                    self.listTitle = self.listSort();
                 };
             },
+            // 排序
+            listSort() {
+                let self = this;
+                return self.listTitle.sort(function (a, b) {
+                    let a1 = a.index;
+                    let b1 = b.index;
+                    a.y = 0;
+                    b.y = 0;
+                    if (a1 < b1) {
+                        return -1;
+                    } else if (a1 > b1) {
+                        return 1;
+                    }
+                    return 0;
+                })
+            },
+        },
+        created() {
         },
         mounted() {
-            console.log(-2 % 20)
             let self = this;
             let writeLeft = self.$refs.writeLeft;
             writeLeft.addEventListener("scroll", function () {
